1b374c84f2217bedcb24c86401324191108599aa
QSmally <github@qbot.eu>
2020-07-03 17:43:18 +0200

    Project main file

---
+++ QDB.js
@@ -0,0 +1,15 @@
+
+/*
+    QDB is built from the ground up by QSmally.
+    QDB © 2020 by QSmally, all rights reserved.
+    Please report bugs by creating an issue on the repo.
+
+    Obviously the Node runtime and other libraries
+    are not created by myself, they're created by
+    their respective owners. All rights are reserved.
+
+    Qulity © 2020 by QSmally, all rights reserved.
+*/
+
+
+module.exports = {};
