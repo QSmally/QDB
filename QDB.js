8eb1bcadf06558487e88b3cc5d11a36fec92e0aa
QSmally <github@qbot.eu>
2020-07-17 15:20:21 +0200

    Added license notices

--- QDB.js
+++ QDB.js
@@ -1,14 +1,14 @@
 
 /*
     QDB is built from the ground up by QSmally.
-    QDB © 2020 by QSmally, all rights reserved.
+    QDB © 2018-2020 by QSmally, all rights reserved.
     Please report bugs by creating an issue on the repo.
 
-    Obviously the Node runtime and other libraries
-    are not created by myself, they're created by
-    their respective owners. All rights are reserved.
+    Obviously the Node runtime (and all respective libraries)
+    are not created by myself, they're created by their
+    owners. All rights are reserved.
 
-    Qulity © 2020 by QSmally, all rights reserved.
+    Qulity © 2020 by QSmally.
 */
 
 
