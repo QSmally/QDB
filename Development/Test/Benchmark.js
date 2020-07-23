3306e66645f6c9b4f84ea6985fe81b1c9cd008a2
QSmally <github@qbot.eu>
2020-07-23 21:14:39 +0200

    Basic tests

--- Development/Test/Benchmark.js
+++ Development/Test/Benchmark.js
@@ -1,5 +1,7 @@
 
 const QDB = require("../../QDB");
-const MyDB = new QDB.Connection("Development/Test/Users.db");
+const MyDB = new QDB.Connection("Development/Test/Users.qdb");
 
 console.log(MyDB.API);
+
+console.log(MyDB.size);
