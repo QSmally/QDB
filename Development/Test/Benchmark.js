6be5bed28836186bff635f454e82f239a6af944b
QSmally <github@qbot.eu>
2020-07-24 13:17:34 +0200

    Tested database managers

--- Development/Test/Benchmark.js
+++ Development/Test/Benchmark.js
@@ -3,8 +3,20 @@ const QDB = require("../../QDB");
 const MyDB = new QDB.Connection("Development/Test/Users.qdb");
 
 console.log(MyDB.API);
+console.log(MyDB.Size);
+
+MyDB.Set("9a51dy", {
+    Name: "QSmally",
+    Age: 19,
+    Hobbies: ["Programming", "Sleeping"]
+});
 
 console.log(MyDB.Size);
+
+console.log(MyDB.Fetch("foo"));
+console.log(MyDB.Fetch("9a51dy"));
+
+console.log(MyDB.Cache);
 
 MyDB.Disconnect();
 
