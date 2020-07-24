cded3e8e4c8844ffc227a183080ff4a8ad448fd2
QSmally <github@qbot.eu>
2020-07-24 16:15:47 +0200

    Tested dotaccess notation

--- Development/Test/Benchmark.js
+++ Development/Test/Benchmark.js
@@ -2,22 +2,16 @@
 const QDB = require("../../QDB");
 const MyDB = new QDB.Connection("Development/Test/Users.qdb");
 
-console.log(MyDB.API);
-console.log(MyDB.Size);
-
 MyDB.Set("9a51dy", {
     Name: "QSmally",
     Age: 19,
     Hobbies: ["Programming", "Sleeping"]
 });
 
-console.log(MyDB.Size);
+MyDB.Set("9a51dy.Age", 20);
 
-console.log(MyDB.Fetch("foo"));
 console.log(MyDB.Fetch("9a51dy"));
 
-console.log(MyDB.Cache);
+console.log(MyDB.Fetch("9a51dy.Hobbies.0"));
 
 MyDB.Disconnect();
-
-console.log(MyDB);
