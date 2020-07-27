30db6eeab0486747901ca9e8bd64e8d720e90d86
QSmally <github@qbot.eu>
2020-07-27 16:08:40 +0200

    Created actual benchmarking setup

--- Development/Test/Benchmark.js
+++ Development/Test/Benchmark.js
@@ -1,25 +1,37 @@
 
 const QDB = require("../../QDB");
-const MyDB = new QDB.Connection("Development/Test/Users.qdb");
 
-MyDB.API.prepare("DELETE FROM 'QDB' WHERE Key = ?").run("8v_65ver85b5");
-MyDB.Set("8v_65ver85b5", {
-    Name: "Molly",
-    Age: 18,
-    Description: "f"
+const Guilds = new QDB.Connection("Development/Test/Guilds.qdb", {
+    // Change this to true/false for (no) caching
+    Cache: false
 });
 
-// console.log({Cached: MyDB.CacheSize, Fetched: MyDB.Size, Rows: MyDB.API.prepare("SELECT * FROM 'QDB'").all(), Cache: MyDB.Cache});
+// START READ TIME
+const Indexes = Guilds.API.prepare("SELECT * FROM 'QDB';").all().map(v => v.Key);
 
-console.log(MyDB.Fetch("8v_65ver85b5"));
-console.log(MyDB.Fetch("p6_fhtkuyxtb"));
-console.log(MyDB.Fetch("b5_lvee9uszh"));
-console.log(MyDB.Fetch("8v_65ver85b5"));
+console.time("time-for-100k-reads");
 
-MyDB.Set("8v_65ver85b5.Age", 200);
+for (let i = 0; i < 100 * 1000; i++) {
+    const Id = Indexes[Math.round(Math.random() * Indexes.length)];
+    if (!Id) continue;
 
-console.log(MyDB.Fetch("8v_65ver85b5"));
+    console.time("per-fetch");
+    const Ft = Guilds.Fetch(Id);
+    console.timeEnd("per-fetch");
+    console.log(Ft);
+}
 
-console.log({Cached: MyDB.CacheSize, Fetched: MyDB.Size, Rows: MyDB.API.prepare("SELECT * FROM 'QDB'").all(), Cache: MyDB.Cache});
+console.log(`cache size: ${Guilds.CacheSize}`);
+console.timeEnd("time-for-100k-reads");
+console.log(`memory usage: ${process.memoryUsage().heapUsed / 1024 / 1024} MB`);
 
-MyDB.Disconnect();
+
+
+// START WRITE
+// TODO: Make benchmark from this
+// for (let i = 0; i < 50000; i++) {
+//     const Id = (Math.random() * 10).toString(36).replace(/\./g, "_");
+//     Guilds.Set(Id, {
+//         foo: "bar"
+//     });
+// }
