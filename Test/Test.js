38c808c346c24321f738ed82cd849251c3c2fe8d
QSmally <github@qbot.eu>
2020-08-22 17:10:16 +0200

    Added tests for Each and Select methods

--- Test/Test.js
+++ Test/Test.js
@@ -106,6 +106,30 @@ module.exports = (QDB, Tap) => {
     Tap("Con#Invert3", Con.Invert("6789.Hobbies.0.Programming"), true);
     Tap("Con#Invert4", Con.Invert("6789.Hobbies.0.Programming"), false);
 
+    let res = [];
+
+    Con.Each((_Row, Key) => {
+        res.push(Key);
+    }, true);
+
+    Tap("Con#Each", res, [...Con.Indexes]);
+
+    Tap("Con#Select", Con.Select((_Row, Key) => {
+        return Key === "3456";
+    }), {
+        "3456": {
+            Name: "roo",
+            Age: 29,
+            Hobbies: [
+                "one",   "two",
+                "three", "-5",
+                "loo",   "1",
+                "2",     "3"
+            ],
+            _DataStore: "3456"
+        }
+    });
+
     Con.Disconnect();
 
 }
