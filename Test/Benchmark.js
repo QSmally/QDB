b52124af1d5094cc56bc0fbf53608c81ac8cdb0d
QSmally <github@qbot.eu>
2020-10-29 14:15:10 +0100

    Remove Pool thread benchmarks

--- Test/Benchmark.js
+++ Test/Benchmark.js
@@ -12,32 +12,6 @@ function Test () {
 
 }
 
-// Million threaded queries benchmark
-async function Thread () {
-    const Pool = new QDB.Pool("Test/Guilds.qdb", {
-        Threaded: true
-    });
-
-    const GuildsThread = Pool.Select("Guilds");
-    console.log("benchmark: fetch 1 million random queries in thread");
-    const Indexes = await GuildsThread.Query("Indexes");
-
-    console.time("time-for-million-reads");
-
-    for (let i = 0; i < 1000 * 1000; i++) {
-        const Id = Indexes[Math.round(Math.random() * Indexes.length)];
-        if (!Id) continue;
-        GuildsThread.Fetch(Id);
-    }
-
-    console.log(`cache size: ${await GuildsThread.Query("CacheSize")}`);
-    console.timeEnd("time-for-million-reads");
-    console.log(`memory usage: ${process.memoryUsage().heapUsed / 1024 / 1024} MB`);
-
-    Pool.Disconnect();
-    if (Disconnect) Pool.Disconnect();
-}
-
 // Million queries benchmark
 function Fetch () {
     const Guilds = new QDB.Connection("Test/Guilds.qdb", {
