d50e6ac316750711317715d27dc8e282c7794e0f
QSmally <github@qbot.eu>
2020-11-22 17:27:50 +0100

    Start of new benchmark interface

---
+++ Benchmark/Main.js
@@ -0,0 +1,8 @@
+
+const CLI = require("cli-color");
+
+process.stdout.write(CLI.reset);
+process.stdout.write(CLI.cyan.bold("Generating tables for benchmark..."));
+// const Connections = require("./Populate")();
+
+process.stdout.write(CLI.reset);
