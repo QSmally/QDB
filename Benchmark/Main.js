775d7389a15ebfea4066d4252e692b3604699668
QSmally <github@qbot.eu>
2020-11-23 17:52:40 +0100

    Formatting changes, add newline by the write end

--- Benchmark/Main.js
+++ Benchmark/Main.js
@@ -2,11 +2,11 @@
 const CLI = require("cli-color");
 
 process.stdout.write(CLI.reset);
-process.stdout.write(CLI.cyan("Generating tables for benchmark..."));
+process.stdout.write(CLI.cyan("Generating tables for benchmark...\n"));
 const Trials = require("./Seed")();
 
-process.stdout.write(CLI.reset);
-process.stdout.write(CLI.cyan.bold("Running benchmarks...\n"));
+// process.stdout.write(CLI.reset);
+// process.stdout.write(CLI.cyan.bold("Running benchmarks...\n"));
 
 // for (const Trial of Trials) {
 
