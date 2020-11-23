9b643fbe7b7f190f4660c60fc040d64132b032ee
QSmally <github@qbot.eu>
2020-11-23 08:14:22 +0100

    Create tables for tests

--- Benchmark/Seed.js
+++ Benchmark/Seed.js
@@ -1,6 +1,14 @@
 
+const Tables = new Map([
+    ["Small", 100],
+    ["Medium", 20000],
+    ["Large", 50000],
+    ["Enterprise", 800000]
+]);
+
 module.exports = () => {
 
+    const Crypto = require("crypto");
     const QDB    = require("../QDB");
     const SQL    = require("better-sqlite3");
     const Master = new SQL("Benchmark/Guilds.qdb");
@@ -11,8 +19,30 @@ module.exports = () => {
     Master.close();
 
     // Create new Connections
-    // for (const [Table, Context] of Tables) {}
+    for (const [Table, Size] of Tables) {
+        const Connection = new QDB.Connection("Benchmark/Guilds.qdb", {
+            Cache: false, Table
+        });
 
-    return [];
+        for (let i = 0; i < Size; i++) {
+            Connection.Set(Crypto.randomBytes(8).toString("hex"), {
+                Username: [
+                    "Jake", "Smally", "Amy", "Foo", "Bar", "Apolly"
+                ][Math.round(Math.random() * 5)],
+
+                Password: Crypto.randomBytes(32).toString("hex"),
+
+                Hobbies: [
+                    ["Sleep", "Programming"],
+                    ["Eating", "Yoga"],
+                    ["Skating"]
+                ][Math.round(Math.random() * 2)]
+            });
+
+            Connection.Disconnect();
+        }
+    }
+
+    return [...Tables.keys()];
 
 }
