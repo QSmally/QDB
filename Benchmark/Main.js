ae17dd539040cb039413695d4ef593ba9d5df53d
QSmally <github@qbot.eu>
2020-11-24 09:54:28 +0100

    Implement time-based benchmark controller

--- Benchmark/Main.js
+++ Benchmark/Main.js
@@ -9,14 +9,24 @@ const Trials = require("./Seed")();
 process.stdout.write(CLI.reset);
 process.stdout.write(CLI.cyan.bold("Running benchmarks...\n"));
 
+const Times = new Map();
+
 for (const Trial of Trials) {
+    const Test = Trial.split(".")[0];
+
     const Benchmark = require(`./Trials/${Trial}`);
     const Connection = new QDB.Connection("Benchmark/Guilds.qdb", {...Benchmark.Options});
-    process.stdout.write(CLI.white(`· Sampling '${CLI.white.bold(Trial.split(".")[0])}' benchmark... `));
+    process.stdout.write(CLI.white(`· Sampling '${CLI.white.bold(Test)}' benchmark...`));
+
+    Times.set(Test, []);
 
     for (let i = 0; i < 10; i++) {
+        const StartTime = process.hrtime();
         Benchmark.Execute(Connection);
+        Times.get(Test).push(process.hrtime(StartTime));
     }
 
     Connection.Disconnect();
 }
+
+console.log(Times);
