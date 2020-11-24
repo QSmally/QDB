ea56544e1df37f2826933bb4f26f7e69df7072b6
QSmally <github@qbot.eu>
2020-11-24 08:02:42 +0100

    Extend interface of benchmarking, execute tests

--- Benchmark/Main.js
+++ Benchmark/Main.js
@@ -1,13 +1,22 @@
 
+const QDB = require("../QDB");
 const CLI = require("cli-color");
 
 process.stdout.write(CLI.reset);
 process.stdout.write(CLI.cyan("Generating tables for benchmark...\n"));
 const Trials = require("./Seed")();
 
-// process.stdout.write(CLI.reset);
-// process.stdout.write(CLI.cyan.bold("Running benchmarks...\n"));
+process.stdout.write(CLI.reset);
+process.stdout.write(CLI.cyan.bold("Running benchmarks...\n"));
 
-// for (const Trial of Trials) {
+for (const Trial of Trials) {
+    const Benchmark = require(`./Trials/${Trial}`);
+    const Connection = new QDB.Connection("Benchmark/Guilds.qdb", {...Benchmark.Options});
+    process.stdout.write(CLI.white(`· Sampling '${CLI.white.bold(Trial.split(".")[0])}' benchmark... `));
 
-// }
+    for (let i = 0; i < 10; i++) {
+        Benchmark.Execute(Connection);
+    }
+
+    Connection.Disconnect();
+}
