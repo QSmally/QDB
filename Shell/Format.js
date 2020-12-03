9f68dac18d46a216cd298005188b224c16d4c1ba
QSmally <github@qbot.eu>
2020-12-03 20:58:22 +0100

    Implement new functions for the formatting manager

--- Shell/Format.js
+++ Shell/Format.js
@@ -1,3 +1,9 @@
 
 const C = {BOLD: "\x1b[1m", RESET: "\x1b[0m"};
-module.exports = Text => `${C.BOLD}${Text}${C.RESET}`;
+
+module.exports = {
+    BOLD: Text => `${C.BOLD}${Text}${C.RESET}`,
+    LIST: Items => Object.keys(Items)
+        .map(Item => `  ${Item}:`.padEnd(8) + Items[Item])
+        .join("\n")
+}
