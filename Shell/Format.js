31cdf4c97789d8a182be56f0377ebb3291506c22
QSmally <github@qbot.eu>
2020-12-03 22:13:33 +0100

    Fix indentation formatting

--- Shell/Format.js
+++ Shell/Format.js
@@ -4,6 +4,6 @@ const C = {BOLD: "\x1b[1m", RESET: "\x1b[0m"};
 module.exports = {
     BOLD: Text => `${C.BOLD}${Text}${C.RESET}`,
     LIST: Items => Object.keys(Items)
-        .map(Item => `  ${Item}:`.padEnd(8) + Items[Item])
+        .map(Item => `  ${Item}:`.padEnd(18) + Items[Item])
         .join("\n")
 }
