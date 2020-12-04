deea830f77eb08838aca4d771fdda7933eaa481c
QSmally <github@qbot.eu>
2020-12-04 09:53:31 +0100

    Edit the amount of padding for the list command

--- Shell/Store/List-tables.js
+++ Shell/Store/List-tables.js
@@ -15,7 +15,7 @@ module.exports = {
         .map(Row => Row.name).map(Table => [Table, Connection.prepare(`SELECT count(*) FROM '${Table}';`).get()["count(*)"]])
         .map(Entry => [Format.BOLD(Entry[0]), `${Entry[1]} rows`]);
 
-        console.log(Format.LIST(Object.fromEntries(Tables), 20));
+        console.log(Format.LIST(Object.fromEntries(Tables), 26));
         Connection.close();
 
         return true;
