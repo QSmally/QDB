843fa37094cc6c9b25189c44874f1c04fe3db7e6
QSmally <github@qbot.eu>
2020-12-04 08:53:55 +0100

    Ask for a table name for the management of tables

--- Shell/Menu.js
+++ Shell/Menu.js
@@ -3,7 +3,7 @@ module.exports = async Path => {
 
     const Selected = await require("./Prompts/Select").run();
     const Command  = require(`./Store/${Selected}`);
-    const Table    = Command.Input ? await require("./Prompts/Table").run() : null;
+    const Table    = Command.Input ? await require("./Prompts/Table")(Command.Action) : null;
 
     return Command.Execute(Path, Table);
 
