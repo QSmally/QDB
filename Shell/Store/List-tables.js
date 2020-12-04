ce9ca58568048b4e1a3ae6c17e4ae1d3f7324dc3
QSmally <github@qbot.eu>
2020-12-04 09:51:02 +0100

    Implement new list-tables action

---
+++ Shell/Store/List-tables.js
@@ -0,0 +1,24 @@
+
+const Format = require("../Format");
+const SQL    = require("better-sqlite3");
+
+module.exports = {
+    Input:       false,
+    Action:      "list",
+    Description: "Lists all the tables in this database file.",
+
+    Execute: Path => {
+
+        const Connection = new SQL(Path);
+        
+        const Tables = Connection.prepare("SELECT name FROM 'sqlite_master' WHERE type = 'table';").all()
+        .map(Row => Row.name).map(Table => [Table, Connection.prepare(`SELECT count(*) FROM '${Table}';`).get()["count(*)"]])
+        .map(Entry => [Format.BOLD(Entry[0]), `${Entry[1]} rows`]);
+
+        console.log(Format.LIST(Object.fromEntries(Tables), 20));
+        Connection.close();
+
+        return true;
+
+    }
+};
