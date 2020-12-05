e86d93dd287529cc751cf01c7aeca0ef2afb4754
QSmally <github@qbot.eu>
2020-12-05 20:24:12 +0100

    Implement dim colour background for errors

--- Shell/Format.js
+++ Shell/Format.js
@@ -1,7 +1,12 @@
 
-const C = {BOLD: "\x1b[1m", RESET: "\x1b[0m"};
+const C = {
+    DIM: "\x1b[40m",
+    BOLD: "\x1b[1m",
+    RESET: "\x1b[0m"
+};
 
 module.exports = {
+    DIM: Text => `${C.DIM}${Text}${C.RESET}`,
     BOLD: Text => `${C.BOLD}${Text}${C.RESET}`,
     LIST: (Items, Padding) => Object.keys(Items)
         .map(Item => `  ${Item}:`.padEnd(Padding) + Items[Item])
