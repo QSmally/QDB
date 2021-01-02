2afb1c52feef51bb811c2735acd9ea55c44eba92
QSmally <github@qbot.eu>
2021-01-02 18:45:22 +0100

    Maintenance: update year number

--- QDB.js
+++ QDB.js
@@ -1,14 +1,14 @@
 
 /*
     QDB is built from the ground up by QSmally.
-    QDB © 2018-2020 by QSmally, all rights reserved.
+    QDB © 2018-2021 by QSmally, all rights reserved.
     Please report bugs by creating an issue on the repo.
 
     Obviously the Node runtime (and all respective libraries)
     are not created by myself, they're created by their
     owners. All rights are reserved.
 
-    Qulity © 2020 by QSmally.
+    Qulity © 2021 by QSmally.
 */
 
 
