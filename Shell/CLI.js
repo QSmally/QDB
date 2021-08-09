f94ad8196b91369fb2abba9e57d79716c03721cc
QSmally <github@qbot.eu>
2021-08-09 12:36:54 +0200

    CLI: fix reserved `arguments` keyword

--- Shell/CLI.js
+++ Shell/CLI.js
@@ -3,27 +3,27 @@
 
 const { existsSync, readdirSync } = require("fs");
 
-const Format    = require("./Format");
-const arguments = process.argv.slice(2);
+const Format = require("./Format");
+const toolArguments = process.argv.slice(2);
 
 const menu = require("./Menu");
 const help = require("./Prompt/Help");
 
-if (!arguments.length) {
+if (!toolArguments.length) {
     help();
 } else {
     const commands = new Map(readdirSync(`${__dirname}/Prompt/`)
         .map(C => [C.split(".")[0].toLowerCase(), require(`./Prompt/${C}`)])
     );
 
-    const action = arguments.shift();
+    const action = toolArguments.shift();
     const executable = commands.get(action.toLowerCase());
-    if (executable) return executable(arguments.shift());
+    if (executable) return executable(toolArguments.shift());
 
     if (!existsSync(action)) return console.log([
         `${Format.dim("Error")}: '${action}' does not exist.`,
         "Use 'qdb make <database>' to create a new database file."
     ].join("\n"));
 
-    menu(action, arguments);
+    menu(action, toolArguments);
 }
