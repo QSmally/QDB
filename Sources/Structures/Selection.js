d769705eb1036a27fdfda61f593e632fccbd0912
QSmally <github@qbot.eu>
2021-10-20 22:35:43 +0200

    Selection: implement `filter`, `limit` and `group` methods

--- Sources/Structures/Selection.js
+++ Sources/Structures/Selection.js
@@ -76,6 +76,67 @@ class Selection {
     // SQL methods
     // ... order, filter, limit, group, join
 
+    /**
+     * Sweeps the values that don't satisfy the provided function.
+     * Identical to the `FILTER BY` SQL statement.
+     * @param {Function} predicate A tester function which returns a boolean based on the properties of the row.
+     * @returns {Selection}
+     */
+    filter(predicate) {
+        for (const [keyContext, document] of this.cache) {
+            if (!predicate(document, keyContext, this))
+                this.cache.delete(keyContext);
+        }
+
+        return this;
+    }
+
+    /**
+     * Slices off values which are out of the bounds of the limit statement.
+     * Identical to the `LIMIT` SQL statement.
+     * @param {Number} [extractionStartOffset] An index to start with filtering the entries, defaults to 0 if only an amount is given.
+     * @param {Number} amount The amount to limit the Selection to. It can be placed at the offset parameter, which then starts at index 0.
+     * @returns {Selection}
+     */
+    limit(extractionStartOffset, amount) {
+        if (!amount) {
+            amount = extractionStartOffset;
+            extractionStartOffset = 0;
+        }
+
+        let pointer = 0;
+
+        for (const [keyContext] of this.cache) {
+            if (extractionStartOffset > pointer && pointer >= extractionStartOffset + amount)
+                this.cache.delete(keyContext);
+            pointer++;
+        }
+    }
+
+    /**
+     * Groups the Selection's values based on a particular property.
+     * Identical to the `GROUP BY` SQL statement.
+     * @param {Pathlike} pathlike Specifies which row or nested property to group by.
+     * @returns {Selection}
+     */
+    group(pathlike) {
+        const [_, path] = Generics.resolveKeyPath(pathlike);
+        const originalSelectionObject = this.cache.toPairObject();
+        this.cache.clear();
+
+        for (const index in originalSelectionObject) {
+            const documentObject = originalSelectionObject[index];
+            const property = Generics.pathCast(documentObject, path);
+            const existingGroup = this.cache.get(property);
+
+            existingGroup ?
+                existingGroup[index] = documentObject :
+                this.cache.set(property, { [index]: documentObject });
+        }
+
+        return this;
+    }
+
     // Utility methods
     // ... map, merge, clone
 }
