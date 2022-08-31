a073e5e25f0cc27ebc49b4042df92b1cb55154e3
QSmally <github@qbot.eu>
2022-08-31 22:20:52 +0200

    Project: add type definitions to enumeration functions

--- Sources/Enumerations/JoinStrategy.js
+++ Sources/Enumerations/JoinStrategy.js
@@ -2,8 +2,13 @@
 module.exports = {
 
     /**
+     * A subroutine to describe the insertion of joined properties.
+     * @typedef {Function} JoinStrategy
+     */
+
+    /**
      * A strategy which flattens any keys into the document itself.
-     * @returns {Function}
+     * @returns {JoinStrategy}
      */
     flatten() {
         return (documentObject, keyContext, value) => {
@@ -14,7 +19,7 @@ module.exports = {
     /**
      * A strategy which assigns all the joined documents to a particular property.
      * @param {String} name The name of the property to insert the joined documents into.
-     * @returns {Function}
+     * @returns {JoinStrategy}
      */
     property(name) {
         return (documentObject, keyContext, value) => {
--- Sources/Enumerations/SortingPredicate.js
+++ Sources/Enumerations/SortingPredicate.js
@@ -2,9 +2,14 @@
 module.exports = {
 
     /**
+     * A subroutine to describe the sorting strategy.
+     * @typedef {Function} SortStrategy
+     */
+
+    /**
      * A strategy which sorts entities in ascending order.
      * @param {Function} selector A function which returns a property to compare with.
-     * @returns {Function}
+     * @returns {SortStrategy}
      */
     ascending(selector) {
         return (node_0, node_1) => selector(node_0) > selector(node_1);
@@ -13,7 +18,7 @@ module.exports = {
     /**
      * A strategy which sorts entities in descending order.
      * @param {Function} selector A function which returns a property to compare with.
-     * @returns {Function}
+     * @returns {SortStrategy}
      */
     descending(selector) {
         return (node_0, node_1) => selector(node_0) < selector(node_1);
