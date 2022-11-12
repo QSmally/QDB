e7a512383b3f4f89d0c228663e4340ee74f7a794
QSmally <github@qbot.eu>
2022-11-12 12:49:26 +0100

    Generics: return input object if there's no casting target

--- Sources/Generics/PathCast.js
+++ Sources/Generics/PathCast.js
@@ -2,15 +2,19 @@
 /**
  * Finds a relative dot-separated pathway of a data model.
  * @param {DataModel} dataObject The object-like target.
- * @param {Array<String>} pathContext A parsed array of a pathlike notation from 'resolveKeyPath'.
+ * @param {Array<String>} path A parsed array of a pathlike notation from 'resolveKeyPath'.
  * @param {*} [item] A value to place into the pathway endpoint.
  * @returns {*}
  */
-module.exports = (dataObject, pathContext, item) => {
-    const originalDataObject = dataObject;
-    const finalKey = pathContext.pop();
+module.exports = (dataObject, path, item) => {
+    if (!path.length && item === undefined) {
+        return dataObject;
+    }
 
-    for (const key of pathContext) {
+    const originalDataObject = dataObject;
+    const finalKey = path.pop();
+
+    for (const key of path) {
         if (typeof dataObject !== "object") return;
         if (!dataObject.hasOwnProperty(key) && item === undefined) return;
         if (!dataObject.hasOwnProperty(key)) dataObject[key] = {};
